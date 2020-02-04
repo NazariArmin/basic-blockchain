364416385a6d15ac45d07c5d63b19a9dedbf430c
RmnNzr <anphoenix1999@gmail.com>
2020-02-04 15:29:42 +0330

    fix some problem

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,24 @@
 from time import time 
-from .proof_of_work import hash
+from proof_of_work import hash
 class Blockchain:
+
     def __init__(self):
-        chain = []
-        mempool = []
-        get_block(first_blcok=True)
-    
+        self.chain = []
+        self.mempool = []
+        first_block = self.get_block(first_block=True)
+        self.add_block(first_block)
+
     def add_block(self, block):
         self.chain.append(block)
         return self.chain
 
-    def get_block(first_block=False):
+    def get_block(self, first_block=False):
         block = {
                     "index": len(self.chain),
                     "transaction": self.mempool,
                     "time": time(),
-                    "bounce" : 0
-                    "previouse_hash": not first_block or hash(self.last_block)
+                    "bounce" : 0,
+                    "previouse_hash": first_block or hash(self.last_block),
                 }   
         return block
     
@@ -28,7 +30,10 @@ class Blockchain:
         }
         self.mempool.append(new_trx)
 
-
+    @property
+    def get_chain(self):
+        print(self.chain)
+        return self.chain
     @property
     def last_block(self):
         return self.chain[-1]
