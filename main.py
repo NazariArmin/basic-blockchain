43fee63c021c579c927c3e8791fa8d0b932db1c6
RmnNzr <anphoenix1999@gmail.com>
2020-02-05 13:44:11 +0330

    add mining end point

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from flask import Flask, jsonify, request
 from blockchain import Blockchain
 from sys import argv
-
+from proof_of_work import proof_of_work
 
 my_block_chain = Blockchain()
 app = Flask(__name__)
 port = int(argv[1])
+
+@app.route('/mine')
+def mine():
+    my_block_chain.new_transaction(
+        sender='base',
+        receiver='armin',
+        amount=25,
+    )
+    new_block = my_block_chain.get_block()
+    new_block = proof_of_work(new_block)
+    my_block_chain.add_block(new_block)
+    return get_chain()
 
 @app.route('/chain')
 def get_chain():
     res = {
         "chain":my_block_chain.get_chain
     }
-    print(res)
     return jsonify(res)
 
 @app.route('/transaction/create', methods=['POST'])
